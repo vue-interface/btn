d00927ea837bcd84b450dd2ab3e4afb7dadbed38
Justin Kimbrell <jkimbrell@actengage.com>
2022-08-09 14:37:00 -0600

    pre-release commit

--- tailwindcss/index.js
+++ tailwindcss/index.js
@@ -273,49 +273,49 @@ module.exports = plugin(function({ addComponents, matchComponents, theme }) {
                 'xs': {
                     padding: '.15rem .35rem',
                     fontSize: '.75rem',
-                    borderRadius: '.2rem',
+                    borderRadius: theme('form.borderRadius') && '.2rem',
                 },
 
                 'sm': {
                     padding: '.25rem .5rem',
                     fontSize: '.875rem',
-                    borderRadius: '.25rem',
+                    borderRadius: theme('form.borderRadius') && '.25rem',
                 },
 
                 'base': {
                     padding: '.375rem .75rem',
                     fontSize: '1rem',
-                    borderRadius: '.25rem',
+                    borderRadius: theme('form.borderRadius') && '.25rem',
                 },
     
                 'lg': {
                     padding: '.5rem 1rem',
                     fontSize: '1.25rem',
-                    borderRadius: '.25rem',
+                    borderRadius: theme('form.borderRadius') && '.25rem',
                 },
     
                 'xl': {
                     padding: '.66rem 1.25rem',
                     fontSize: '1.33rem',
-                    borderRadius: '.33rem',
+                    borderRadius: theme('form.borderRadius') && '.33rem',
                 },
     
                 '2xl': {
                     padding: '.75rem 1.5rem',
                     fontSize: '1.5rem',
-                    borderRadius: '.5rem',
+                    borderRadius: theme('form.borderRadius') && '.5rem',
                 },
     
                 '3xl': {
                     padding: '.85rem 1.75rem',
                     fontSize: '1.75rem',
-                    borderRadius: '.5rem',
+                    borderRadius: theme('form.borderRadius') && '.5rem',
                 },
     
                 '4xl': {
                     padding: '1rem 2rem',
                     fontSize: '2rem',
-                    borderRadius: '.66rem',
+                    borderRadius: theme('form.borderRadius') && '.66rem',
                 }
             }
         })
