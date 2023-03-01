ab9d8b96a62e3992e991e6b7ad89dbae9ed4cb3a
Justin Kimbrell <jkimbrell@actengage.com>
2023-03-01 17:21:47 -0500

    updated vite config

--- vite.config.js
+++ vite.config.js
@@ -3,29 +3,30 @@ import { pascalCase } from 'change-case';
 import path from 'path';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
-import { name } from './package.json';
+import pkg from './package.json';
 
-const fileName = name.split('/')[1];
+const fileName = pkg.name.split('/')[1];
+
+const external = [
+    ...(pkg.dependencies ? Object.keys(pkg.dependencies) : []),
+    ...(pkg.peerDependencies ? Object.keys(pkg.peerDependencies) : [])
+];
 
 export default defineConfig({
     build: {
         lib: {
             entry: path.resolve(__dirname, 'index.ts'),
             name: pascalCase(fileName),
-            fileName
+            fileName,
         },
         rollupOptions: {
-            external: [
-                '@vue-interface/sizeable',
-                '@vue-interface/variant',
-                'vue'
-            ],
+            external,
             output: {
-                globals: {
-                    '@vue-interface/sizeable': 'Sizeable',
-                    '@vue-interface/variant': 'Variant',
-                    'vue': 'Vue'
-                },
+                globals: external.reduce((carry, dep) => {
+                    return Object.assign(carry, {
+                        [dep]: pascalCase(dep)
+                    });
+                }, {}),
             }
         },
         watch: !process.env.NODE_ENV && {
